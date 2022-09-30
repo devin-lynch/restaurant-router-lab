c39892c849c5a7f5ef053b3e8eead998c84fcd6e
devin-lynch <devin.l.lynch@gmail.com>
2022-09-30 16:07:25 -0500

    Add basic styling

--- src/components/pages/Menu.jsx
+++ src/components/pages/Menu.jsx
@@ -2,12 +2,12 @@ export default function Menu() {
     return (
         <div>
             <h1>Menu:</h1>
-            <ol>
-                <li>Bacon and Egg Tacos</li>
-                <li>Asada Mini Tacos</li>
-                <li>Pastor Quesadilla</li>
-                <li>Fideo</li>
-            </ol>
+                <ol style={{listStyle: 'none'}}>
+                    <li>Bacon and Egg Tacos</li>
+                    <li>Asada Mini Tacos</li>
+                    <li>Pastor Quesadilla</li>
+                    <li>Fideo</li>
+                </ol>
         </div>
     )
 }
