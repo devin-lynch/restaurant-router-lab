5e97cca2b683300da1c383018fd8de328a9cd9eb
devin-lynch <devin.l.lynch@gmail.com>
2022-09-30 15:45:13 -0500

    Add components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+
+import Home from "./components/pages/Home"
+import Menu from "./components/pages/Menu"
+import About from "./components/pages/About"
+import Contact from "./components/pages/Contact"
+
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      App
+      <Home />
+      <Menu />
+      <About />
+      <Contact />
     </div>
   );
 }
 
-export default App;
+
